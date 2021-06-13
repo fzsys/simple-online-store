7a90b446cabc98d61cc1548c042dd7604e93c4a1
igor uhan <igoruhan@nxte.nl>
2021-06-13 09:57:24 +0300

    added error handler, controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+class UserController {
+    async registartion(req, res) {
+
+    }
+
+    async login(req, res) {
+
+    }
+
+    async check(req, res) {
+        res.json('test')
+    }
+}
+
+module.exports = new UserController()
