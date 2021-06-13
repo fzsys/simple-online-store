314e863ee199f9ee58cb4fd98937a2aeea72ff8e
igor uhan <igoruhan@nxte.nl>
2021-06-13 09:47:27 +0300

    added controllers

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const router = require('./routes/index')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
 const start = async () => {
     try {
+        console.log(1)
         await sequelize.authenticate()
         await sequelize.sync()
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
@@ -15,3 +22,5 @@ const start = async () => {
         console.log(e)
     }
 }
+
+start()
